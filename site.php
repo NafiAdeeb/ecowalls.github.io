41667cb3bc28264d62e60085c2af8db6cd73788d
NafiAdeeb <78759645+NafiAdeeb@users.noreply.github.com>
2021-02-12 15:19:34 +0600

    First Website

---
+++ site.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>EcoWalls</title>
+<center><h1 style='color:66ff99'>EcoWalls</h1></center>
+</head>
+<body background="background.jpg">
+<?php
+echo "<h1 style='color:66ff99'><Font face='Century Gothic'>hello guys</h1>"
+
+ ?>
+</body>
+</html>
